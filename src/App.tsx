e7d7c05f825091136664e75146ac1825e78e69bc
danetroup <danetroup@gmail.com>
2025-08-04 18:18:31 -0400

    import path issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 // src/App.tsx
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 // Layout & Context
 import DefaultLayout from './layouts/DefaultLayout';
